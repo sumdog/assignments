c8b8451b7fdb1fc0f04012a1b734bffa8f13c5c4
skhanna <>
2003-02-14 04:44:04 +0000

    #5, palandome, fully works

--- csc3400-program1/src/prob5.c
+++ csc3400-program1/src/prob5.c
@@ -9,10 +9,12 @@
 
 /**\fn short p5testPalindrome(char* s)
  *Checks to see if null terminated string is
- *a palindrone.
+ *a palindrone. Also skips over puncuation
+ *and invalid characters (ASCII 0 to 65 and
+ *122 to 127).
  */
 short p5testPalindrome(char* s) {
-
+    printf("");
   //loops to check the beginng and end
   //for "bad char" (spaces, puncuation, etc)
   while(1) {
@@ -24,20 +26,23 @@ short p5testPalindrome(char* s) {
     }
   }
   while(1) {
-    if ( ((int) s[strlen(s)-1]) < 65 || ((int) s[strlen(s)-1]) > 122 ) {
+    if( strlen(s) == 1 ) {
+      break;
+    }
+    else if ( ((int) s[strlen(s)-1]) < 65 || ((int) s[strlen(s)-1]) > 122 ) {
       s[strlen(s)-1] = '\0';
     }
     else {
       break;
     }
   }
-  
   if( strlen(s) <= 1) { //base case
     return 1;
   }
   else if( *s == s[strlen(s)-1]) {
     s[strlen(s)-1] = '\0';
     s++;
+    //start recursive calls
     return p5testPalindrome(s);
   }
   else { //did not reach base case
