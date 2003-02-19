fd01b04d1f6d434365fbde1c61bbb29485b91c9c
skhanna <>
2003-02-19 10:51:09 +0000

    added some stuff..yet to test it on gemini

--- csc3620-program2/prog2.cbl
+++ csc3620-program2/prog2.cbl
@@ -3,9 +3,10 @@
 * Sumit Khanna
 *  This program will take in
 *  the files 'bank{1,2,3}.dat', sort
-*  each file and then merge them into
-*  one big sorted output file called
-*  'bank.out'
+*  each file, merge them and then
+*  place them in output files 
+*  "{a,b,k,other}.out", based on the
+*  account-type
 *************************************
 
 identification division.
@@ -16,25 +17,25 @@ author.     Sumit Khanna.
 environment division.
 
 configuration section.
-source-computer. GNU-Linux-IA32.
+source-computer. Alpha-VMS.
 
 input-output section.
 file-control.
-    select data-file1    assign to "bank1.dat".
-    select data-file2    assign to "bank2.dat".
-    select data-file3    assign to "bank3.dat".
+    select data-file     assign to " ".
     select sort-temp     assign to "sort.swp".
 data division.
 
 file section.
-fd data-file1 record contains 50 characters.
-01 data-record    pic x(50).
+fd data-file record contains 50 characters
+        value of id is input-filename.
+01 data-record.
+    05 acct-name   pic x(25).
+    05 acct-num    pic 9(6).
+    05 acct-type   pic x(1).
+    05 acct-bal    pic 9(8)v999.
+    05 trans-count pic 9(4).
+    05 state       pic x(4).
 
-fd data-file2 record contains 50 characters.
-01 data-record    pic x(50).
-
-fd data-file3 record contains 50 characters.
-01 data-record    pic x(50).
 
 sd sort-temp  record contains 50 characters.
 01 sort-temp-record.
@@ -47,19 +48,38 @@ sd sort-temp  record contains 50 characters.
 
 working-storage section.
  
-01 data-input.
-    05 acct-name   pic x(25).
-    05 acct-num    pic 9(6).
-    05 acct-type   pic x(1).
-    05 acct-bal    pic 9(8)v999.
-    05 trans-count pic 9(4).
-    05 state       pic x(4).
+01 file-info
+    05 input-filename pic x(50).
+    05 input-eof-flag pic x(1) value "N".
+       88 end-of-file          value "Y".
 
+01 counters.
+    05 i           pic 9(1) usage is comp.
 
 procedure division.
 
 main-paragraph.
 
+    perform sort-paragraph varying i from 1 by 1 until i > 3
+
     stop run.
 
 
+sort-paragraph.
+    
+    if i = 1 then
+       move "bank1.dat" to input-filename.
+    if i = 2 then
+       move "bank2.dat" to input-filename.
+    if i = 3 then
+       move "bank3.dat" to input-filename.
+
+    open input data-file.
+    read data-file at end move "Y" to input-eof-flag.
+    perform sort-input-proc until end-of-file.    
+    close data-file.
+
+sort-input-proc.
+    
+
+sort-output-proc.
