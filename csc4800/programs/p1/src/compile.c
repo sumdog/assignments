88fb3661ccdb6db8f6b8267888a058e8883c66ef
sumdog <>
2004-09-08 01:06:15 +0000

    Program #1 works and is ready to turn in

--- csc4800/programs/p1/src/compile.c
+++ csc4800/programs/p1/src/compile.c
@@ -1,18 +1,26 @@
 /* put necessary includes here */
-
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int compile(char *src, char* out) {
+  
 
-	/* remove outfile.txt */
+  /* create outfile or if it exists
+     open it and truncate it to 0 */
+  freopen("outfile.txt","w+",stderr);
+ 
+  pid_t child = fork();
+  if(child) { /* parent */
+    freopen("/dev/tty","a",stderr);
+    int state;
+    waitpid(child,&state,0);
+    return state;
+  }
+  else { /* child */
+    return execl("/usr/bin/gcc","gcc",src,"-o",out,NULL);
+  }
 
-	/* call freopen to make standard error point to outfile.txt */
-	/* for a child */
-	
-	/* Child: call execl to run gcc */
-	
-	/* Parent: call freopen (on /dev/tty) to restore standard error */
-
-	/* Parent: wait for child */
-
-	/* Return 0 for success, or not zero for failure */
+  
 }
