d2ee35d5e5994742408402617cba609c0d45835d
sumdog <>
2004-12-09 01:12:27 +0000

    Added notes and updates program #5

--- csc4800/programs/p5/program5.php
+++ csc4800/programs/p5/program5.php
@@ -1,6 +1,7 @@
 <?php
+
   function create_input($name,$size) {
-    echo "<input type=\"text\" value=\"{$_POST[$name]}\" name=\"$name\" />";
+    echo "<input type=\"text\" size=\"$size\" value=\"{$_POST[$name]}\" name=\"$name\" />";
   }
 ?>
 <!DOCTYPE >
@@ -11,11 +12,37 @@
 <body>
 
 <?php
- if( !isset($_GET['register']) ) {
+ if( isset($_GET['register']) ) {
+   
 ?>
 <h1>Please Register</h1>
 <form method="post" action="program5.php">
-
+ <?php
+      echo "First";
+      create_input("fname",20);
+      echo "Last";
+      create_input("lname",20);
+      echo "<br />";
+      echo "SSN";
+      create_input("ssn1",3);
+      create_input("ssn2",2);
+      create_input("ssn3",4);
+      echo "<br />";
+      echo "Address";
+      create_input("add1",20);
+      echo "<br />";
+      echo "Address";
+      create_input("add2",20);
+      echo "<br />";
+      echo "City";
+      create_input("city",10);
+      echo "State";
+      create_input("state",2);
+      echo "<br />";
+      echo "Email";
+      create_input("email",20);
+ ?>
+<input type="submit" value="Register" />
 </form>
 <?php
  } else {
