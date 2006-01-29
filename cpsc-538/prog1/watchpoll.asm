8d9272080195e3aa485a52b4bbc72f7c82650a6a
battlepe <>
2006-01-29 18:44:19 +0000

    Made a lot of changes, however I think the Linux compiler is buggy Moving to windows machine

--- cpsc-538/prog1/watchpoll.asm
+++ cpsc-538/prog1/watchpoll.asm
@@ -1,15 +1,13 @@
-  ORG	  $0800
 
 ;Define Function
-GETCHAR EQU	$FE02
-PUTCHAR EQU	$F684
+;GETCHAR EQU	$FE02
+;PUTCHAR EQU	$F684
 
 ;Define Memory I/O
-BOOLIN	EQU	$00C4
-DATAIN	EQU	$00C7
-
-BOOLOUT EQU     $102E
-DATAOUT EQU     $102F
+SREADY	 EQU	$00C4
+SDATA 	 EQU	$00C7
+SINMASK  EQU    $0020
+SOUTMASK EQU    $0080
 
 ;Define ASCII 
 KEYSS   EQU     $0053 ;S
@@ -20,12 +18,16 @@ KEYRR   EQU     $0052 ;R
 KEYR    EQU     $0072 ;r
 KEYGG   EQU     $0047 ;G
 KEYG    EQU     $0067 ;g
+KEYBS   EQU     $0008 ;(backspace)
 
+  ORG	  $0800
+  
 ;define datatypes
 COUNT	   DW	   0
 LAP	   DW	   0
 CLOCKON    DB      0
 LAPON      DB      0
+
 
 ;Primary Loop for program
 ;   1 - Check for any key actions
@@ -34,19 +36,25 @@ LAPON      DB      0
 ;   4 - Loop
 Main:
             jsr KeyPoll
+	    jsr AdjustTime
 	    jsr ClearScreen
             jsr PrintTime
             jsr Wait
             bra Main
 
+;Prints Register D to screen
+PutChar:
+            brclr SREADY,#SOUTMASK,PutChar
+	    stab SDATA
+            rts
 
 ;Poll for a command and if present, call
 ; appropiate function 
 ; (clears register B)
 KeyPoll:
             clrb                        
-            brclr BOOLIN,$0020,key_ret ;Check to see if we have input
-	    ldab DATAIN                ;Read in input
+            brclr SREADY,#SINMASK,key_ret ;Check to see if we have input
+	    ldab SDATA                 ;Read in input
             cmpb #KEYSS                ;Start comparison of keys
             beq StopClock
             cmpb #KEYS
@@ -69,18 +77,38 @@ key_ret                                ;no input or unrecognized key
 ;Prints enough backspaces to clear
 ; the counter
 ClearScreen:
-            nop
+            ldd KEYBS
+            ldab $07                   ;a = 7
+	    dbeq b,clear_done          ;while a < 7; a--
+	    jsr PutChar 
+clear_done:
 	    rts
 
 ;Prints the output
-;  If(
 PrintTime:
-            nop
+            ldaa LAPON                 ;A = (bool) lapon
+	    cmpa #$1                   ;if (A = on)
+	    beq show_lap               ; goto show_lap
+            ldd COUNT                  ;else load count
+	    bra show
+show_lap:
+            ldd LAP
+show:
+	    jsr PutChar
+	    rts
+	    
+;Preforms time adjustments
+AdjustTime:
+            ldaa CLOCKON               ;A = (bool) clockon
+	    cmpa #$0                   ;if(A = off)
+	    beq clock_off              ;return
+            ldd COUNT                  ;else increment counter
+	    addd $0001
+	    std COUNT
+clock_off:
 	    rts
 
 ;Loops nops for the remainder of time
-;  if(CLOCKON) { increment COUNT }
-;  if(LAPON)   { increment LAP }
 Wait:
             nop
             rts
@@ -89,22 +117,29 @@ Wait:
 
 ;G/g - Starts the clock
 StartClock:
+            ldaa #$01
+	    staa CLOCKON
             rts
 
 ;L/l - Starts or Stops the lap
 StartStopLap:
-            nop
             rts
 
 ;S/s - Stops the clock
 StopClock:
-            nop 
+            ldaa #$00
+	    staa CLOCKON
             rts
 
 ;R/r - Resets the counter 
 ;      (ignored of counter is running)
 RestClock:
-           nop
-	   rts
+            ldaa CLOCKON
+	    cmpa #$01
+	    beq reset_ignore
+	    ldd #$0000
+	    std COUNT 
+reset_ignore:
+	    rts
 
 ;END KEYBOARD FUNCTIONS--------------
