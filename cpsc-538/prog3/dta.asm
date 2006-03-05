6372e846ee3ffc9bd98eecd82a439d2e4b947856
battlepe <>
2006-03-05 22:27:27 +0000

    Hex number is being output to device Control signals to ATD need work

--- cpsc-538/prog3/dta.asm
+++ cpsc-538/prog3/dta.asm
@@ -17,20 +17,44 @@ LF      EQU  $0A
 ;I/O Ports
 PORTT   EQU  $AE
 DDRT    EQU  $AF
+PORTE   EQU  $08
+DDRE    EQU  $09
 
 
    ORG $0800
 
 Main:
+   ldaa #$FF               ;Set PORT E for writing
+   staa DDRE
+main_loop:
    jsr ReadInput
    jsr sciDisplayNum
-   bra Main
+   jsr sendDAC
+   bra main_loop:
 
 sciDisplayNum:
    ldd HEXINPUT
    pshd
    ldd #DFORMAT
    jsr [PRINTF,PCR]
+   puld
+   rts
+
+getADC:
+   ldaa #$00               ;Set PORTT for reading
+   staa DDRT
+
+   rts
+
+sendDAC:
+   ldaa #$FF               ;Set PORTT for writing
+   staa DDRT
+   ldaa #$00               ;Reset DAC
+   staa PORTE
+   ldd  HEXINPUT           ;send data to converter
+   stab PORTT
+   ldaa #$FF               ;Tell DAC to update registers
+   staa PORTE
    rts
 
 ReadInput:
@@ -60,5 +84,5 @@ IFORMAT  DB  "Input Hex Number: ",0
 EFORMAT  DB  CR,LF,"Invalid Number",CR,LF,0
 DFORMAT  DB  CR,LF,"Sending %X",CR,LF,0
 RFORMAT  DB  "Recieved %X",CR,LF,0
-HEXINPUT DW  $0000
+HEXINPUT DW  $00
 INBUF    DS  $03
