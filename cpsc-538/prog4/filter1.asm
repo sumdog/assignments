6d5c28a227e545acdfa5094b536638eaabc7e932
battlepe <>
2006-03-28 21:59:30 +0000

    Added support for user input to select frequency

--- cpsc-538/prog4/filter1.asm
+++ cpsc-538/prog4/filter1.asm
@@ -26,10 +26,11 @@
 ;
 
 ;Define D-Bug 12 Function
-PRINTF  EQU  $F686
 SETUVEC EQU  $F69A
+PRINTF  EQU  $F686
+GETCHAR EQU  $-------
 
-;ASCII
+;ASCII constants
 CR      EQU  $0D
 LF      EQU  $0A
 
@@ -56,14 +57,30 @@ TEN     EQU  %10000000 ; Timer enable bit
 C7F     EQU  %10000000 ; Output compare 2 Flag
 C7I     EQU  C7F       ; Interrupt enable
 IOS7    EQU  %10000000 ; Select OC2
-;TCRE    EQU  %00001101
+TCRE    EQU  %00001100 ; Prescale by 16 and enable Timer Compare 7
 
 
-;Prescaler / TC7 Compare Value 
+;The following blocks contain
+; the sample rates and coefficents
+; needed for conversion at that rate.
+; Uncomment the revelant block of code 
+; in order to change sample rates
 
-;10kHz / Prescale by 16 and compare to 50
-TCRE    EQU  %00001100
-CMP7    EQU  $32 
+;10kHz (Compare TC7 to 50)
+;CMP7    EQU  $32 
+;COX     EQU  $02DA
+;CONX    EQU  $FD27
+
+;20kHz
+;CMP7    EQU  $19
+;COX     EQU  $0356
+;CONX    EQU  $FCAB
+
+;50kHz
+;CMP7    EQU  $0A
+;COX     EQU  $03AB
+;CONX    EQU  $FC56
+
 
    ORG $0800
 
@@ -76,11 +93,52 @@ Main:
    ldaa #%11110111         ;Set Busy read, rest write
    staa DDRE
    ldaa #%10010000         ;Set PORTE to normal I/O mode
+   
    staa PEAR
    jsr InitalizeTimer
 main_loop:
    ;wai
    bra main_loop
+
+
+;--Sets frequency and coefficents
+AskFreq:
+   ldd #AFORMAT
+   jsr [PRINTF,PCR]
+   jsr [GETCHAR,PCR]
+   
+   cmpb #$31
+   beq feq10
+   cmpb #$32
+   beq feq20
+   cmpb #$33
+   beq feq50
+   
+   ldd #EFORMAT
+   jsr [PRINTF,PCR]
+   bra AskFreq
+   
+feq10:
+   ldda #$32
+   ldd #$02DA
+   ldx #$FD27
+   bra setfeq
+feq20:
+   ldda #$19
+   ldd #$0356
+   ldx #$FCAB
+   bra setfeq
+feq50:
+   ldaa #$0A
+   ldd #$03AB
+   ldx #$FC56
+
+setfeq:
+   sta CMP7
+   std COX
+   stx CONX
+   rts
+
 
 ;--Called by Main procedure to start Timer
 InitalizeTimer:
@@ -106,7 +164,7 @@ InitalizeTimer:
    staa TMSK2
 
    ;Set TC7 (p187) -- This adjusts our time interval
-   ldd  #CMP7
+   ldd  CMP7
    std  TC7
 
    ;Mask to enable TEN in TSCR (p153) (DO THIS LAST)
@@ -136,34 +194,13 @@ waitForConversion:
    anda #%00001000
    cmpa #%00001000
    beq complete
-   ;ldd #BFORMAT
-   ;jsr [PRINTF,PCR]
    bra waitForConversion
 complete:
-   ;ldd #CFORMAT
-   ;jsr [PRINTF,PCR]
    rts
-
-;--Displays number recieved from ATD
-sciReadNum:
-   ldd HEXOUT
-   pshd
-   ldd #RFORMAT
-   jsr [PRINTF,PCR]
-   puld
-   rts
-
-;--Displays number sent to DTA
-sciDisplayNum:
-   ldd HEXINPUT 
-   pshd
-   ldd #DFORMAT
-   jsr [PRINTF,PCR]
-   puld
-   rts
+   
 
 ;--Retrieves a voltage from the ATD
-;   stores result in HEXOUT
+;   stores result in SIGIN
 getADC:
    ldaa #$00               ;Set PORTT for reading
    staa DDRT
@@ -187,7 +224,7 @@ getADC:
 
    ;read from pins
    ldaa PORTT
-   staa HEXOUT
+   staa SIGIN
 
    ;set all control signal back to high
    ldaa #$FF
@@ -204,7 +241,7 @@ sendDAC:
    staa PORTE
 
 
-   ldd  HEXINPUT           ;send data to converter
+   ldd  SIGOUT           ;send data to converter
    stab PORTT
 
 
@@ -218,18 +255,18 @@ sendDAC:
 
 ;--Function to handle filtering of input
 filter:
-   ldd   HEXOUT       ;Load value we brought in
-   tab
-   ldaa #$00   
+   ldd   SIGIN       ;Load value we brought in
+   tab               ;move high bits to low bits
+   ldaa #$00         ;zero out high bits
 
    ;multiple current input by negative coeffcent
-   ldy #$FD26
+   ldy CONX
    emuls
    std TMPF
    
    ;multiple previous output by positive coeffecent
    ldd POUT
-   ldy #$02DA
+   ldy COX
    emuls
 
    ;Add them together
@@ -241,24 +278,19 @@ filter:
 
    ;this will be previous out and current out
    stx POUT
-   stx HEXINPUT
+   stx SIGOUT
 
-   
-
-   ;ldaa POUT         ;Load previous Output
-
-
-
-   staa POUT         ;Store output for next cycle
-   std  HEXINPUT     ;Filtered Value to send Back Out
    rts
 
 ;Variables
-DFORMAT  DB  CR,LF,"Sending %X",CR,LF,0
-RFORMAT  DB  "Recieved %X",CR,LF,0
-;CFORMAT  DB  "Conversion Complete",CR,LF,0
-;BFORMAT  DB  "Busy",CR,LF,0
-HEXINPUT DW  $00  ;DTA Input value to send
-HEXOUT   DW  $00  ;ATD Output value recieved
-POUT     DW  $00  ;Previous Output
-TMPF     DW  $00  ;Temp 16-storage for formula
+SIGOUT  DW  $00  ;DTA Input value to send
+SIGIN   DW  $00  ;ATD Output value recieved
+POUT    DW  $00  ;Previous Output
+TMPF    DW  $00  ;Temp 16-storage for formula
+
+AFORMAT DB "Frequency:",CR,LF,"1)10kHz",CR,LR,"2)20kHz",CR,LF,"3)50kHz",CR,LF,0
+EFORMAT DB "Invalid",CR,LF,0
+CMP7    DB $00
+COX     DW $0000
+CONX    DW $0000
+
