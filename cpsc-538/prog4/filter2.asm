c867644a5abdb816b1ad095a4ede2fd868974c86
battlepe <>
2006-04-03 23:38:46 +0000

    switched filter 2 to a delay loop Interrurpts don't work!!!

--- cpsc-538/prog4/filter2.asm
+++ cpsc-538/prog4/filter2.asm
@@ -26,11 +26,7 @@
 ;    in parallel
 ;   
 ;
-;  10kHz Sampling 
-;  (Prescaler of 32 / Timer Compare 25 ($19)
-
-;Define D-Bug 12 Function
-SETUVEC EQU  $F69A
+;  2.5kHz Sampling (delay loop)
 
 ;ASCII constants
 CR      EQU  $0D
@@ -43,32 +39,17 @@ PORTE   EQU  $08
 DDRE    EQU  $09
 PEAR    EQU  $0A
 
-;Timer Constants
-TIMER7  EQU  $10       ; Timer7 for D-Bug12 SetUserVector()
-TIOCS   EQU  $80       ; In capt/out compare select
-TSCR    EQU  $86       ; Timer System Control Register (p179)
-TCTL1   EQU  $88       ; Timer In/Out Pins
-TCTL2   EQU  $89
-TCTL3   EQU  $8A
-TCTL4   EQU  $8B
-TMSK1   EQU  $8C       ; Timer mask reg
-TMSK2   EQU  $8D       ; Timer Interrupt Mask 2 Register (p182)
-TFLG1   EQU  $8E       ; TFLG1 offset
-TC7     EQU  $9E       ; Timer compare register 7 (p187)
-TEN     EQU  %10000000 ; Timer enable bit
-C7F     EQU  %10000000 ; Output compare 2 Flag
-C7I     EQU  C7F       ; Interrupt enable
-IOS7    EQU  %10000000 ; Select OC2
-TCRE    EQU  %00001101 ; Enable Timer Compare 7
-
 
    ORG $0800
 
 ;--Main Entry Point
 ;   * Set read/write modes for ports
 ;   * Set PORTE to be a normal (vs specalized) port
-;   * Setup Timers
-;   * Wait
+;   * Read Input
+;   * Filter
+;   * Send output
+;   * Delay
+;   * Loop
 Main:
    ldaa #%11110111         ;Set Busy read, rest write
    staa DDRE
@@ -85,59 +66,23 @@ Main:
    staa POUT
    staa PPOUT
 
-   jsr InitalizeTimer
 main_loop:
-   bra main_loop
-
-
-
-;--Called by Main procedure to start Timer
-InitalizeTimer:
-   ;Setup Timer Interrupt Handler
-   ldd  #IntTime
-   pshd
-   ldd  #TIMER7
-   ldx  SETUVEC
-   jsr 0,x
-   puld
-
-   bset TIOCS,IOS7  ;Bit to enable IOS7 in TIOS (p151)
-
-   ;Disable I/O pins
-   ldaa #$0
-   staa TCTL1
-   staa TCTL2
-   staa TCTL3
-   staa TCTL4
-
-   ;Enable TCRE and set Prescaler of 32 TMASK2 (p182,p183)
-   ldaa #TCRE
-   staa TMSK2
-
-   ;Set TC7 (p187) -- This adjusts our time interval
-   ldd  #$0064
-   std  TC7
-
-   ;Mask to enable TEN in TSCR (p153) (DO THIS LAST)
-   ldaa  #C7F
-   staa  TFLG1	          ; Clear C7F
-   bset  TMSK1,C7I       ; Enable TC7 Interrupt
-   ;Enable the Timer system
-   bset  TSCR,TEN
-   cli                   ; Unmask global interrupts
-   rts
-
-;BEGIN Interurpt Functions---------------
-
-;--Called by Timer Interrupt
-; * Calls input function
-; * Runs filtering 
-; * Calls output function
-IntTime:
    jsr getADC
    jsr filter
    jsr sendDAC
-   rti
+   jsr Delay
+   jsr Delay
+   jsr Delay
+   bra main_loop
+
+;--Delay loop
+;  (run 3 times to get 2500Hz samples
+Delay:
+   ldaa #$FF
+d2: 
+   deca
+   bne d2
+   rts
 
 
 ;--Retrieves a voltage from the ATD
