100b892e94087053077e0484a824b5f09761e30e
battlepe <>
2006-09-21 01:18:01 +0000

    Assignment #4 is done

--- cpsc-542/assignments/assignment4/xml.js
+++ cpsc-542/assignments/assignment4/xml.js
@@ -25,30 +25,22 @@
       http_request.send(null);
    }
 
-   function sendPost(url, data) {
-      http_request = false;
-      if (window.XMLHttpRequest) { // Mozilla, Safari,...
-         http_request = new XMLHttpRequest();
-         if (http_request.overrideMimeType) {
-            http_request.overrideMimeType('text/xml');
-         }
-      } else if (window.ActiveXObject) { // IE
-         try {
-            http_request = new ActiveXObject("Msxml2.XMLHTTP");
-         } catch (e) {
-            try {
-               http_request = new ActiveXObject("Microsoft.XMLHTTP");
-            } catch (e) {}
-         }
-      }
-      if (!http_request) {
-         alert('Cannot create XMLHTTP instance');
-         return false;
-      }
-      http_request.onreadystatechange = alertContents;
-      http_request.open('POST', url, true);
-      http_request.send(data);
+
+   function alterText(xmldoc,na,nb,nc,docnode) {
+      var a = xmldoc.getElementsByTagName(na).item(0);
+      var b = a.getElementsByTagName(nb).item(0);
+      var c = b.getElementsByTagName(nc).item(0);
+      var tnode = xmldoc.createTextNode(document.getElementById(docnode).value);
+      c.appendChild(tnode);
    }
+
+   function alterUTCID(xmldoc,na,docnode) {
+      var a = xmldoc.getElementsByTagName(na).item(0);
+      var b = a.getElementsByTagName('utcid').item(0);
+      var tnode = xmldoc.createTextNode(document.getElementById(docnode).value);
+      b.appendChild(tnode);
+   }
+
 
    function alertContents() {
       if (http_request.readyState == 4) {
@@ -57,55 +49,60 @@
             var xmldoc = http_request.responseXML;
             var root = xmldoc.getElementsByTagName('application').item(0);
 
-            //var pre = document.createElement('pre'); 
-            //pre.appendChild(document.createTextNode(http_request.responseText)); 
-            //document.body.appendChild(pre);
-            sendPost('http://zog.utc.edu/~ssmullen/cgi-bin/echoxml.pl',http_request.responseText)
+            alterUTCID(xmldoc,'namechange','utcid');
+            alterUTCID(xmldoc,'addresschange','addrutcid');
 
-            //document.write(http_request.responseText); 
-            /*for (var iNode = 0; iNode < root.childNodes.length; iNode++) {
-               var node = root.childNodes.item(iNode);
-               for (i = 0; i < node.childNodes.length; i++) {
-                  var sibl = node.childNodes.item(i);
-                  var len = parseInt(sibl.childNodes.length / 2);
-                  var arr = new Array(len);
-                  var cnt = 0;
-                  for (x = 0; x < sibl.childNodes.length; x++) {
-                     var sibl2 = sibl.childNodes.item(x);
-                     var sibl3;
-                     if (sibl2.childNodes.length > 0) {
-                        sibl3 = sibl2.childNodes.item(0);
-                        arr[cnt] = sibl3.data;   
-                        cnt++;
-                     }
-                  }
-                  addrow("mytable", arr);
-               }
-            }*/
+            alterText(xmldoc,'namechange','current','first','cfirst');
+            alterText(xmldoc,'namechange','current','middle','cmiddle');
+            alterText(xmldoc,'namechange','current','last','clast');
+            alterText(xmldoc,'namechange','current','maiden','cmaiden');
+            alterText(xmldoc,'namechange','new','first','nfirst');
+            alterText(xmldoc,'namechange','new','middle','nmiddle');
+            alterText(xmldoc,'namechange','new','last','nlast');
+            alterText(xmldoc,'namechange','new','maiden','nmaiden');
+
+            alterText(xmldoc,'addresschange','name','first','afirst');
+            alterText(xmldoc,'addresschange','name','middle','amiddle');
+            alterText(xmldoc,'addresschange','name','last','alast');
+            alterText(xmldoc,'addresschange','name','maiden','amaiden');
+
+            alterText(xmldoc,'addresschange','permanentaddr','street','perstreet');
+            alterText(xmldoc,'addresschange','permanentaddr','city','percity');
+            alterText(xmldoc,'addresschange','permanentaddr','state','perstate');
+            alterText(xmldoc,'addresschange','permanentaddr','zip','perzip');
+            alterText(xmldoc,'addresschange','permanentaddr','phone','perphone');
+
+            alterText(xmldoc,'addresschange','presentaddr','street','prestreet');
+            alterText(xmldoc,'addresschange','presentaddr','city','precity');
+            alterText(xmldoc,'addresschange','presentaddr','state','prestate');
+            alterText(xmldoc,'addresschange','presentaddr','zip','prezip');
+            alterText(xmldoc,'addresschange','presentaddr','phone','prephone');
+
+            var xmlout = "";
+            if (window.XMLHttpRequest) { // Mozilla, Safari,...
+              var serializer = new XMLSerializer();
+              xmlout = serializer.serializeToString(xmldoc);
+            } else {
+              xmlout = xmldoc.xml;
+            }
+            
+            /*
+            var pre = document.createElement('pre'); 
+            pre.appendChild(document.createTextNode(http_request.responseText)); 
+            pre.appendChild(document.createTextNode(xmlout));
+            document.body.appendChild(pre);
+            */
+
+            if (confirm( xmlout ) ) {
+              document.getElementById('val').value = xmlout;
+              document.getElementById('mainform').submit();
+            }
+
          } else {
             alert('There was a problem with the request.');
          }
       }
    }
    function do_xml() {
-      makeRequest('app.xml', '?test=2');
+      makeRequest('app.xml', '');
    }
-   function addrow(tablename, arr) {
-   var tbl = document.getElementById(tablename);
-   var lastRow = tbl.rows.length;
-   var row = tbl.insertRow(lastRow);
-      for (r = 0; r < arr.length; r++) {   
-         var cell = row.insertCell(r);
-         cell.innerHTML = arr[r];
-      }
-   }
-
-//<input type="button" name="button" value="GET XML" 
-//   onclick="javascript:do_xml();">
-
-//Table filled with data requested from the server:<br>
-//<table border="1" id="mytable">
-//</table>
-
-
-
