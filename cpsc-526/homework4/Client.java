096cd431a971f3c9ca468f7f18af9b4d6291414a
battlepe <>
2006-11-14 04:30:03 +0000

    updated homework #4 finished my presentation Keynote and CVS don't get along

--- cpsc-526/homework4/Client.java
+++ cpsc-526/homework4/Client.java
@@ -149,17 +149,17 @@ public class Client{
     class setupButtonListener implements ActionListener{
 	public void actionPerformed(ActionEvent e){
 
-	    //System.out.println("Setup Button pressed !");      
+	    System.out.println("Setup Button pressed !");      
 
 	    if (state == INIT) 
 		{
 		    //Init non-blocking RTPsocket that will be used to receive data
 		    try{
 			//construct a new DatagramSocket to receive RTP packets from the server, on port RTP_RCV_PORT
-			//RTPsocket = ...
+		    RTPsocket = new DatagramSocket(RTP_RCV_PORT);
 
 			//set TimeOut value of the socket to 5msec.
-			//....
+			RTPsocket.setSoTimeout(5);
 
 		    }
 		    catch (SocketException se)
@@ -180,8 +180,8 @@ public class Client{
 		    else 
 			{
 			    //change RTSP state and print new state 
-			    //state = ....
-			    //System.out.println("New RTSP state: ....");
+			    state = READY;
+			    System.out.println("New RTSP state: READY");
 			}
 		}//else if state != INIT then do nothing
 	}
@@ -192,12 +192,12 @@ public class Client{
     class playButtonListener implements ActionListener {
 	public void actionPerformed(ActionEvent e){
 
-	    //System.out.println("Play Button pressed !"); 
+	    System.out.println("Play Button pressed !"); 
 
 	    if (state == READY) 
 		{
 		    //increase RTSP sequence number
-		    //.....
+		    RTSPSeqNb++;
 
 
 		    //Send PLAY message to the server
@@ -209,8 +209,8 @@ public class Client{
 		    else 
 			{
 			    //change RTSP state and print out new state
-			    //.....
-			    // System.out.println("New RTSP state: ...")
+			    state = PLAYING;
+			    System.out.println("New RTSP state: PLAYING");
 
 			    //start the timer
 			    timer.start();
@@ -225,12 +225,12 @@ public class Client{
     class pauseButtonListener implements ActionListener {
 	public void actionPerformed(ActionEvent e){
 
-	    //System.out.println("Pause Button pressed !");   
+	    System.out.println("Pause Button pressed !");   
 
 	    if (state == PLAYING) 
 		{
 		    //increase RTSP sequence number
-		    //........
+		    RTSPSeqNb++;
 
 		    //Send PAUSE message to the server
 		    send_RTSP_request("PAUSE");
@@ -241,8 +241,8 @@ public class Client{
 		    else 
 			{
 			    //change RTSP state and print out new state
-			    //........
-			    //System.out.println("New RTSP state: ...");
+			    state = READY;
+			    System.out.println("New RTSP state: READY");
 			          
 			    //stop the timer
 			    timer.stop();
@@ -257,10 +257,10 @@ public class Client{
     class tearButtonListener implements ActionListener {
 	public void actionPerformed(ActionEvent e){
 
-	    //System.out.println("Teardown Button pressed !");  
+	    System.out.println("Teardown Button pressed !");  
 
 	    //increase RTSP sequence number
-	    // ..........
+	    RTSPSeqNb++;
       
 
 	    //Send TEARDOWN message to the server
@@ -272,8 +272,8 @@ public class Client{
 	    else 
 		{     
 		    //change RTSP state and print out new state
-		    //........
-		    //System.out.println("New RTSP state: ...");
+		    state = INIT;
+		    System.out.println("New RTSP state: INIT");
 
 		    //stop the timer
 		    timer.stop();
@@ -385,15 +385,19 @@ public class Client{
 	    //Use the RTSPBufferedWriter to write to the RTSP socket
 
 	    //write the request line:
-	    //RTSPBufferedWriter.write(...);
+	    RTSPBufferedWriter.write(request_type);
 
 	    //write the CSeq line: 
-	    //......
+	    RTSPBufferedWriter.write("CSeq: "+RTSPSeqNb);
 
 	    //check if request_type is equal to "SETUP" and in this case write the Transport: line advertising to the server the port used to receive the RTP packets RTP_RCV_PORT
-	    //if ....
+	    if(request_type.equals("SETUP")) {
+	    	RTSPBufferedWriter.write("Transport: RTP/UDP; client_port= "+ RTP_RCV_PORT);
+	    }
 	    //otherwise, write the Session line from the RTSPid field
-	    //else ....
+	    else {
+	    	RTSPBufferedWriter.write("Session: "+RTSPid);
+	    }
 
 	    RTSPBufferedWriter.flush();
 	}
