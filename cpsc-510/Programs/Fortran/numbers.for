82c93e9c39192bbaad5cd772dfd67cb6b2c318dd
battlepe <>
2007-01-31 22:17:43 +0000

    Added the start to the numbers program for Fortran

---
+++ cpsc-510/Programs/Fortran/numbers.for
@@ -0,0 +1,12 @@
+C  Fortran program to take input of
+C  n numbers, determine average
+C  and number of inputs above average
+C  -Sumit Khanna - cpsc-510/Thompson
+C
+C2345678901234567890123456789
+C Initalize all Vars and Formats 
+       INTEGER INUMVALUES(1), IAVERAGE, ICOUNT, IABOVE
+C Read user input
+       READ * , INUMVALUES
+       INTEGER INPUT(10)
+       END
