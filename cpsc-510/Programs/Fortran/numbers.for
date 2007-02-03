4803c2f24c5f39078628899ac8c5f523eec75d13
battlepe <>
2007-02-03 18:34:55 +0000

    Finished Fortran Program

--- cpsc-510/Programs/Fortran/numbers.for
+++ cpsc-510/Programs/Fortran/numbers.for
@@ -5,8 +5,23 @@ C  -Sumit Khanna - cpsc-510/Thompson
 C
 C2345678901234567890123456789
 C Initalize all Vars and Formats 
-       INTEGER INUMVALUES(1), IAVERAGE, ICOUNT, IABOVE
+       DIMENSION A(1)
+       ISUM=0
+       IABOVE=0
+       IAVERAGE=0
 C Read user input
-       READ * , INUMVALUES
-       INTEGER INPUT(10)
+       PRINT *, 'Enter the total number of integers and then <enter>'
+       PRINT *, 'Then enter each integer, one per line'
+       READ *,N,(A(I),I=1,N)
+       DO 100 X=1,N,1
+       ISUM=ISUM+A(X)
+100    CONTINUE
+C Perform Calculations
+       IAVERAGE=ISUM/N
+       DO 200 Y=1,N,1
+       IF ( A(Y) .GT. IAVERAGE ) IABOVE=IABOVE+1
+200    CONTINUE
+C Show Output
+       PRINT *,'Average: ',IAVERAGE
+       PRINT *,'Numbers Above Average: ',IABOVE
        END
